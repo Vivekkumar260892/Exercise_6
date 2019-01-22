360bf597bf6e94695fabe29680734485bc92b8f7
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-22 04:31:22 -0500

    Exercise 6

--- Exercise_6/Program.cs
+++ Exercise_6/Program.cs
@@ -6,7 +6,23 @@ namespace Exercise_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input any Number");
+            String Input = Console.ReadLine();
+            int a = Convert.ToInt32(Input);
+            int i, j, k;
+            for (i = a; i >= 1; i--)
+            {
+                for (j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = a; k >= i; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
